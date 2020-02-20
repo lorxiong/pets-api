8f8c585e03de94c5984067ef137b35127be69d61
Xiong <lor.xiong@c02yd68njg5h.corp.pgcore.com>
2020-02-20 09:51:21 -0600

    pets-api initial commit

---
+++ manage.py
@@ -0,0 +1,20 @@
+# Set the path where the file is
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import manager server from flask_script
+from flask_script import Manager, Server
+from application import create_app
+app = create_app()
+manager = Manager(app)
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = os.getenv('IP', '0.0.0.0'),
+    port = int(os.getenv('PORT', 5000)))
+)
+
+if __name__ == "__main__":
+    manager.run()
---
+++ settings.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'secret_key'
+DEBUG = True
+MONGODB_HOST = 'mongodb' # use 'mongodb' if using Docker
+MONGODB_DB = 'petstore-api'
